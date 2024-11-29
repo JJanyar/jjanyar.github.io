eb12a62bff3e121a28f973b25463a667a413aab2
Janyar Muslem <96023403+JJanyar@users.noreply.github.com>
2024-11-29 08:40:56 +0100

    Portfolio

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-        // reset bij elke refresh zodat de start engine button niet het zelfde blijft.
+// reset bij elke refresh zodat de start engine button niet ingedrukt blijft.
         window.onload = function() {
             document.getElementById('engineButton').checked = false;
         };
 
         document.querySelector('.container input').addEventListener('change', function() {
             if (this.checked) {
-                // speelt de carengine alsje op het knop drukt.
-                const engineSound = document.getElementById('engineSound');
-                engineSound.currentTime = 0; // Reset sound to start
+                // speelt de carengine sound alsje op het knop drukt.
+                const engineSound = document.getElementById('engineSound'); // Pakt de engineSound var van html (gelinkt aan carengine.mp3).
+                engineSound.currentTime = 0; // Reset
                 engineSound.play();
 
-                // Stop the sound after 3 seconds
+                // stopt de start up sound, want daarna rijd hij weg
                 setTimeout(() => {
                     engineSound.pause();
                     engineSound.currentTime = 0;
-                }, 3400);
+                }, 3400); // Hoelang de geluid afspeelt - 3.4 seconde
 
-                // speelt de animatie alsje op het knop drukt.
-                document.querySelector('.circle').classList.add('push-animation');
+                // speelt de animatie alsje op het knop drukt
+                document.querySelector('.circle').classList.add('push-animation'); // Pakt de gegevens van css voor de animatie
                 
                 setTimeout(() => {
                     document.querySelector('.loading-screen').style.opacity = '0';
@@ -28,14 +28,14 @@
                 }, 500);
             }
         });
-
+        // Alsje op mijn profiel foto klikt, verschijnt er een menu.
         document.querySelector('.profile-picture').addEventListener('click', function() {
-            document.querySelector('.profile-info').classList.toggle('hidden');
+            document.querySelector('.profile-info').classList.toggle('hidden'); // Verandert "hidden" de tegenovergestelde
         });
 
-        // Add this event listener to close the profile info when clicking outside
+        // Alsje op het achtergrond klikt, dan maakt hij de menu weer hidden.
         document.querySelector('.profile-info').addEventListener('click', function(event) {
-            if (event.target === this) { // Check if the click is on the background
-                this.classList.add('hidden'); // Hide the profile info
+            if (event.target === this) { // Bekijkt of het de achtergrond is of niet
+                this.classList.add('hidden');
             }
         });
